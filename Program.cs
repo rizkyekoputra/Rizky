0353a7c7c7c2ab1c3ca3dd0158b721a429d5475d
Rizky Eko Putra <rizky.eko.putra@gmail.com>
2016-07-01 11:06:21 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string referal_text = @"[{""name"":""Anggi"",""referral_used"" : ""Frila""},
+                        {""name"" : ""Boboy"", ""referral_used"" : ""Anggi""},
+                        {""name"" : ""Caca"", ""referral_used"" : ""Anggi""},
+                        {""name"" : ""Dea"", ""referral_used"" : ""Boboy""},
+                        {""name"" : ""Ebi"", ""referral_used"" : ""Boboy""},
+                        {""name"" : ""Frila"", ""referral_used"" : ""Caca""}]";
+
+            
+
+            Console.ReadKey();
+        }
+
+        public class User
+        {
+            public User(string json)
+            {
+                User deserial =
+            }
+
+            string name { get; set; }
+            string referral_used { get; set; }
+        }
+        
+    }
+}
